8972677b177201f0364f5c89a82b488cb4ee8a44
Austin Rosenbaum <austinjrosenbaum@gmail.com>
2018-04-10 09:13:44 -0600

    created fillMap method to populate spots

--- main.py
+++ main.py
@@ -11,26 +11,52 @@ def selectColumn(seq):
     val = random.choice(seq)
     return val
 
+def fillMap():
+    # put the player down first
+    # Get player name
+    name = input("What is your name? >> ")
+    if(name.lower() == "jake"):
+        print("Sorry, you can't play.")
+        exit()
+    player = Player(name)
+
+    coinFlip = random.random() * 2
+
+    if(coinFlip < 1):
+        row = selectRow([0,1,2,3,4])
+        column = selectColumn([0,4])
+    else:
+        column = selectColumn([0,1,2,3,4])
+        row = selectRow([0,4])
+
+    print(f"Row: {row + 1}   Column: {column + 1}")
+
+    cur = map[row][column]
+    cur.enter()
+
+    # 6 Easy enemies
+    # 5 Medium enemies
+    # 3 Hard enemies
+    for i in range(6):
+        Enemy(EnemyType.EASY)
+    for i in range(5):
+        Enemy(EnemyType.MEDIUM)
+    for i in range(3):
+        Enemy(EnemyType.HARD)
+
+    # 3 Recharge stations
+    # 2 coffee shops
+    # 3 fun spots
+    # 3 empty
+
+    return {"Spot": cur, "Player": player}
+
 
 map = GameMap.makeMap()
 
-# Get player name
-name = input("What is your name? >> ")
-player = Player(name)
-
-coinFlip = random.random() * 2
-
-if(coinFlip < 1):
-    row = selectRow([0,1,2,3,4])
-    column = selectColumn([0,4])
-else:
-    column = selectColumn([0,1,2,3,4])
-    row = selectRow([0,4])
-
-print(f"Row: {row + 1}   Column: {column + 1}")
-
-cur = map[row][column]
-cur.enter()
+dict = fillMap()
+cur = dict["Spot"]
+player = dict["Player"]
 
 GameMap.printMap(map)
 
